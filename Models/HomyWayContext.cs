b00fed5242cd3e755a68cabedbca847b456bd4c8
yash sodavadiya <yash@gmail.com>
2025-04-07 09:26:35 +0530

    setup project in my laptop

--- Models/HomyWayContext.cs
+++ Models/HomyWayContext.cs
@@ -27,15 +27,19 @@ public partial class HomyWayContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=Keyur\\SQLEXPRESS01;Initial Catalog=HomyWay;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-757AVSO\\SQLEXPRESS;Initial Catalog=HomyWay;Integrated Security=True;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Group>(entity =>
         {
-            entity.ToTable("groups");
+            entity
+                .HasNoKey()
+                .ToTable("groups");
 
-            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
             entity.Property(e => e.Name).HasColumnName("name");
         });
 
@@ -127,7 +131,9 @@ public partial class HomyWayContext : DbContext
         {
             entity.ToTable("users");
 
-            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
                 .HasColumnName("email");
